b3d59c2b0b3a2de032d1ad6f840fdb359101d999
miguelgil7 <miguelgil7@hotmail.com>
2026-02-11 16:52:18 +0000

    generador de nombres miguel gil

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const pronoun = ["I", "you", "he", "she", "it", "we", "they"];
+const adj = ["smart","fast","bright","fresh","cool","wild","super","prime"];
+const noun = ["jogger","racoon","hub","lab","zone","spot","flow","loop","wave","nest"];
+
+function nuevaWeb(i, j, k) {
+  const list = document.querySelector("#list");
+  const liElement = document.createElement("li");
+
+  liElement.textContent = pronoun[i] + adj[j] + noun[k] + ".com";
+  list.appendChild(liElement);
+}
+
+window.onload = function () {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        nuevaWeb(i, j, k);
+      }
+    }
+  }
+};
